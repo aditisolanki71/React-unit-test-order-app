bef7749be559eed4c523ba82097ee2d8d589d3bd
Unknown <adtsolanki22@gmail.com>
2022-11-26 15:42:45 +0530

    Checkbox nd Button

---
+++ src/pages/summary/SummaryForm.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+const SummaryForm = () => {
+  const [checked, setChecked] = useState(false);
+
+  const checkboxLabel = (
+    <span>
+      I agree to <span style={{ color: 'blue' }}>Terms and Conditions</span>
+    </span>
+  );
+  return (
+    <>
+      <Form>
+        <Form.Group controlId="terms-and-conditions">
+          <Form.Check
+            type="checkbox"
+            checked={checked}
+            onChange={(e) => setChecked(e.target.checked)}
+            label={checkboxLabel}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit" disabled={!checked}>
+          Confirm Order
+        </Button>
+      </Form>
+    </>
+  );
+};
+export default SummaryForm;
